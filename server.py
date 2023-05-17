3240e25a0b15869104e1615213b16017ff4d301d
Manas-Ram <68700139+Manas-Ram@users.noreply.github.com>
2023-05-16 23:50:18 -0400

    Completed line graphs page

--- server.py
+++ server.py
@@ -1,17 +1,72 @@
 # flask --app data_server run
 from flask import Flask
+from flask import request
 from flask import render_template
 import json
 
 
-app = Flask(__name__, static_url_path='', static_folder='static')
+app = Flask(__name__, static_url_path="", static_folder="static")
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    f = open("data/life_expectancy.json", "r")
+    data = json.load(f)
+    f.close()
+    canadas = []
+    for e in data["Canada"].values():
+        canadas.append(e)
+    uss = []
+    for e in data["United States"].values():
+        uss.append(e)
+    mexicos = []
+    for e in data["Mexico"].values():
+        mexicos.append(e)
+    points1 = []
+    for i in range(len(canadas) - 1):
+        points1.append([canadas[i], canadas[i + 1]])
+    points2 = []
+    for i in range(len(uss) - 1):
+        points2.append([uss[i], uss[i + 1]])
+    points3 = []
+    for i in range(len(mexicos) - 1):
+        points3.append([mexicos[i], mexicos[i + 1]])
 
-@app.route('/year')
+    ages = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    years = [1960, 1970, 1980, 1990, 2000, 2010, 2020]
+
+    return render_template(
+        "index.html",
+        years=data["Canada"].keys(),
+        canada=points1,
+        us=points2,
+        mexico=points3,
+        ages=ages,
+        years2=years,
+    )
+
+
+@app.route("/year")
 def year():
-    return render_template('year.html')
+    f = open("data/life_expectancy.json", "r")
+    data = json.load(f)
+    f.close()
+
+    year = request.args.get("year")
+
+    mexico = 90 - ((data["Mexico"][year]) - 50) * 2
+    canada = 90 - ((data["Canada"][year]) - 50) * 2
+    us = 90 - ((data["United States"][year]) - 50) * 2
+
+    years = [
+        ["Mexico", data["Mexico"][year]],
+        ["Canada", data["Canada"][year]],
+        ["United States", data["United States"][year]],
+    ]
+
+    return render_template(
+        "year.html", year=year, mexico=mexico, us=us, canada=canada, years=years
+    )
+
 
 app.run(debug=True)
